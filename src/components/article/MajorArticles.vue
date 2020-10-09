337480a42e6990c83f6373c9ce4eb0e7f001734a
dev_eccentric <lefugdot@gmail.com>
2020-10-10 00:51:23 +0300

    Split into separate components

---
+++ src/components/article/MajorArticles.vue
@@ -0,0 +1,38 @@
+<template>
+
+   <div class="flex flex-col items-start">
+
+      <MajorArticle
+         v-for="(article, i) in articles"
+         :key="article.id"
+         :article="article"
+         :index="i + 1"
+      ></MajorArticle>
+
+   </div>
+
+</template>
+
+<script lang="ts">
+
+   import { Component, Prop, Vue } from "nuxt-property-decorator";
+   import MajorArticle from './MajorArticle.vue';
+
+   @Component({
+      name: "MajorArticles",
+      components: {
+         MajorArticle,
+      },
+   })
+   export default class MajorArticles extends Vue {
+
+      articles = [
+
+         { id: 10, title: 'Something here', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et suscipit tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et suscipit tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit'] },
+         { id: 20, title: 'Some other stuff here', content: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et suscipit tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et suscipit tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit'] },
+
+      ]
+
+   }
+
+</script>
---
+++ src/components/other/MainTitle.vue
@@ -0,0 +1,42 @@
+<template>
+   
+   <div class="px-2 md:px-4">
+      
+      <span class="block text-center uppercase text-gray-300 font-light leading-tight">
+         {{ content.subtitle }}
+      </span>
+      <span class="block text-center text-5xl capitalize font-black tracking-wide leading-tight text-gray-200">
+         {{ content.title }}
+      </span>
+
+      <div v-if="divider" class="divider relative h-px my-5 min-w-full">
+         <div class="div-transparent  absolute top-0 left-1/20 right-1/20 h-px">
+            <div class="div-dot absolute rounded-full bg-gray-tertiary cursor-pointer transition transform hover:scale-75 duration-500 linear"></div>
+         </div>
+      </div>
+
+      <p class="text-center font-light text-gray-300">
+         {{ content.paragraph }}
+      </p>
+   
+   </div>
+
+</template>
+
+<script lang="ts">
+
+   import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+   @Component({
+      name: "MainTitle",
+      components: {
+      },
+   })
+   export default class MainTitle extends Vue {
+
+      @Prop() private content;
+      @Prop({ default: true }) private divider?: boolean;
+
+   }
+
+</script>
