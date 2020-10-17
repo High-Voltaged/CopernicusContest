49000f51be6bab6104b1954f08b537e19fe8dfb6
zx2512 <59429199+zx2512@users.noreply.github.com>
2020-10-17 22:20:26 +0200

    Update database.ts

--- database/database.ts
+++ database/database.ts
@@ -2,7 +2,7 @@ import MariaDB from "mariadb";
 
 const pool = MariaDB.createPool({
     host: '127.0.0.1',
-    port: 3308,
+    port: 3306,
     user: 'root',
     password: '',
     database: 'copernicus',
