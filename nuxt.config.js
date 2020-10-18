3705b95deedcc5a9fde39d4a584921b2951e7f7b
zx2512 <59429199+zx2512@users.noreply.github.com>
2020-10-18 17:42:10 +0200

    Add spacing to configuration files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,50 @@
 const pkg = require('./package');
 
 module.exports = {
+
     ssr: false,
+
     telemetry: true,
+
     srcDir: 'src/',
+
     extension: ['js', 'ts'],
+
     server: {
+
         host: '127.0.0.1',
         port: 1337,
+
     },
+
     head: {
+
         title: "Copernicus_project",
+
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
             { hid: "description", name: "description", content: "NTEST" }
         ],
+
         link: [
             { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
         ],
+
     },
+
     loading: { color: "#3B8070" },
+
     css: [
         './src/assets/css/style.scss',
         './src/assets/fonts/poppins.css',
     ],
+
     buildModules: [
         '@nuxtjs/router',
         '@nuxtjs/tailwindcss',
     ],
+
     modules: [
         "@nuxtjs/axios",
         '~/modules/typescript',
