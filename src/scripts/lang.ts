d680258de73a32804704cb8e15982ff62933dbd9
zx2512 <59429199+zx2512@users.noreply.github.com>
2020-10-30 14:03:28 +0100

    Add lang file

---
+++ src/scripts/lang.ts
@@ -0,0 +1,176 @@
+export namespace English {
+
+    export const username: string = "Username";
+
+    export const password: string = "Password";
+
+    export const take_a_quiz_here: string = "Take A Quiz Here";
+
+    export const date_published: string = "Date published";
+
+    export const times_viewed: string = "Times viewed";
+
+    export const edit_articles: string = "Edit articles";
+
+    export const edit_quiz: string = "Edit the quiz";
+
+    export const edit_categories: string = "Edit the categories";
+
+    export const next: string = "Next";
+
+    export const previous: string = "Previous";
+
+    export const save_the_question: string = "Save the Question";
+
+    export const question_out_of: string = "Question {{{1 }}} out of ";
+
+    export const select_category: string = "Select a category";
+
+    export const admin_menu: string = "Admin Menu";
+
+    export const all_articles: string = "All articles";
+
+    export const unsaved_question_confirm: string = "Are you sure you want to leave the question unsaved?";
+
+    export const unsaved_category_confirm: string = "Are you sure you want to leave this category unsaved?";
+
+    export const articles_configuration: string = "Articles configuration";
+
+    export const add_an_article: string = "Add an article";
+
+    export const discard_changes: string = "Discard changes";
+
+    export const save_changes: string = "Save changes";
+
+    export const changes_saved_article: string = "Your changes to the article were saved.'";
+
+    export const changes_discarded_article: string = "Your changes to the article were discarded.";
+
+    export const dont_leave_imput_empty: string = "Don't leave the input fields empty.";
+
+    export const article_title_too_short: string = "The article's title's length is too short.";
+
+    export const article_title_too_long: string = "The article's title's length is too long.";
+
+    export const article_content_too_short: string = "The article's content's length is too short.";
+
+    export const article_content_too_long: string = "The article's content's length is too long.";
+
+    export const article_picture_link_invalid: string = "The article's picture's link is invalid.";
+
+    export const are_you_sure_remove_article: string = "Are you sure you want to remove this article?";
+
+    export const article_title: string = "Article's title";
+
+    export const cancel: string = "Cancel";
+
+    export const add_article: string = "Add article";
+
+    export const article_content: string = "Article's content";
+
+    export const article_category: string = "Article's category";
+
+    export const article_picture_link: string = "Article's picture link";
+
+    export const article_will_be_saved_added: string = "Your article will be saved and added.";
+
+    export const article_content_will_be_discarded: string = "Your article's content will be discarded.";
+
+    export const other_articles: string = "Other Articles";
+
+    export const random_categories: string = "Random categories";
+
+    export const login_to_admin_panel: string = "Login to Admin Panel";
+
+    export const login_details_incorrect: string = "Login details incorrect";
+
+}
+
+// TRANSLATE THIS
+export namespace Polish {
+
+    export const username: string = "Username";
+
+    export const password: string = "Password";
+
+    export const take_a_quiz_here: string = "Take A Quiz Here";
+
+    export const date_published: string = "Date published";
+
+    export const times_viewed: string = "Times viewed";
+
+    export const edit_articles: string = "Edit articles";
+
+    export const edit_quiz: string = "Edit the quiz";
+
+    export const edit_categories: string = "Edit the categories";
+
+    export const next: string = "Next";
+
+    export const previous: string = "Previous";
+
+    export const save_the_question: string = "Save the Question";
+
+    export const question_out_of: string = "Question {{{1 }}} out of ";
+
+    export const select_category: string = "Select a category";
+
+    export const admin_menu: string = "Admin Menu";
+
+    export const all_articles: string = "All articles";
+
+    export const unsaved_question_confirm: string = "Are you sure you want to leave the question unsaved?";
+
+    export const unsaved_category_confirm: string = "Are you sure you want to leave this category unsaved?";
+
+    export const articles_configuration: string = "Articles configuration";
+
+    export const add_an_article: string = "Add an article";
+
+    export const discard_changes: string = "Discard changes";
+
+    export const save_changes: string = "Save changes";
+
+    export const changes_saved_article: string = "Your changes to the article were saved.'";
+
+    export const changes_discarded_article: string = "Your changes to the article were discarded.";
+
+    export const dont_leave_imput_empty: string = "Don't leave the input fields empty.";
+
+    export const article_title_too_short: string = "The article's title's length is too short.";
+
+    export const article_title_too_long: string = "The article's title's length is too long.";
+
+    export const article_content_too_short: string = "The article's content's length is too short.";
+
+    export const article_content_too_long: string = "The article's content's length is too long.";
+
+    export const article_picture_link_invalid: string = "The article's picture's link is invalid.";
+
+    export const are_you_sure_remove_article: string = "Are you sure you want to remove this article?";
+
+    export const article_title: string = "Article's title";
+
+    export const cancel: string = "Cancel";
+
+    export const add_article: string = "Add article";
+
+    export const article_content: string = "Article's content";
+
+    export const article_category: string = "Article's category";
+
+    export const article_picture_link: string = "Article's picture link";
+
+    export const article_will_be_saved_added: string = "Your article will be saved and added.";
+
+    export const article_content_will_be_discarded: string = "Your article's content will be discarded.";
+
+    export const other_articles: string = "Other Articles";
+
+    export const random_categories: string = "Random categories";
+
+    export const login_to_admin_panel: string = "Login to Admin Panel";
+
+    export const login_details_incorrect: string = "Login details incorrect";
+
+}
